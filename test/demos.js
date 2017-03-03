b9465b7c1993d33260c9c23facfcf880ac957cc3
Erik Rose <erik@mozilla.com>
2017-03-03 14:55:31 -0500

    Add tests to show the clustering works sanely when paragraphish things contain other paragraphish things.

--- test/demos.js
+++ test/demos.js
@@ -182,7 +182,7 @@ describe('Design-driving demos', function () {
             // notices).
             const bestClust = max(clustsAndSums, clustAndSum => clustAndSum[1])[0];
             const sortedBest = domSort(bestClust);
-            return sortedBest.map(p => p.textContent.trim().substr(0, 20));
+            return sortedBest.map(p => p.textContent.trim().substr(0, 20).trim());
             // Other ideas: We could pick the cluster around the highest-scoring
             // node (which is more like what Readability does) or the highest-
             // scoring cluster by some formula (num typed nodes * scores of the
@@ -226,6 +226,60 @@ describe('Design-driving demos', function () {
                               'Sid dreamed of dough',
                               'Did you ever trade a',
                               'Hamstrung by ham-han']);
+        });
+
+        it('large inner clusters with some piddly little text in their container', function () {
+            // Feel free to change what you want out of this, but this exists
+            // to show that we thought it a sane output at one point.
+            // distance() considers inner things almost infinitely far from
+            // outer so we end up with either-or: generally just the outer,
+            // which are often more plentiful. We then get the inner by means
+            // of having their containers.
+            const doc = jsdom(`
+                <div>
+                    Smoo bars.
+                    <p>
+                        One <code>fish</code>
+                    </p>
+                    <p>
+                        Two <code>fish</code>
+                    </p>
+                    <p>
+                        Three
+                    </p>
+                    Yo.
+                </div>
+            `);
+            assert.deepEqual(snippetsFrom(doc),
+                             ['One fish', 'Two fish', 'Three']);
+        });
+
+        it('large outer clusters with some piddly inner things contained', function () {
+            // For instance, if the inner things were <code> blocks and the
+            // outer were <p>s, we'd want the <p>s.
+            const doc = jsdom(`
+                <div>
+                    Smoo bars.
+                    <p>
+                        One
+                    </p>
+                    <p>
+                        Two
+                    </p>
+                    <p>
+                        Three
+                    </p>
+                    Yo.
+                </div>
+                <div>
+                    Mangaroo. Witches and kangaroo.
+                </div>
+                <div>
+                    Bing bang, I saw the whole mang.
+                </div>
+            `);
+            assert.deepEqual(snippetsFrom(doc),
+                             ['Smoo bars.', 'Mangaroo. Witches an', 'Bing bang, I saw the']);
         });
     });
 });
