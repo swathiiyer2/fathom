12615776aec969cb6fb51eb46298d02365c42e09
Erik Rose <erik@mozilla.com>
2017-03-13 15:21:01 -0400

    Powell's minimization.
    It gets stuck in local minima, which we can prove by setting the initial vector to all 0s and observing that it never hits 40%, which it did happen to reach with a different initial vector. We need a more aggressive method that works better on stepwise functions. The small perturbations this uses get it nowhere, so it doesn't try larger jumps. Maybe something like simulated annealing or Bayesian optimization? Continuous methods like hill-climbing or gradient descent will not avail us.

--- test/demos.js
+++ test/demos.js
@@ -7,6 +7,7 @@ const {jsdom} = require('jsdom');
 
 const {clusters, distance} = require('../clusters');
 const {dom, out, props, rule, ruleset, score, type} = require('../index');
+const {minimize_Powell} = require('../optimization');
 const {domSort, inlineTextLength, linkDensity, max, numberOfMatches, page, sum} = require('../utils');
 
 
@@ -130,88 +131,92 @@ describe('Design-driving demos', function () {
 
         // ---------------------- The actual algorithm: -----------------------
 
-        // Score a node based on how much text is directly inside it and its
-        // inline-tag children.
-        function scoreByLength(fnode) {
-            const length = inlineTextLength(fnode.element);
-            return {
-                score: length,  // May be scaled someday
-                note: {inlineLength: length}  // Store expensive inline length for linkDensity().
-            };
-        }
+        function contentNodesWithCoefficients(coeffLinkDensity = 1, coeffParagraphTag = 1.5, coeffLength = 1, coeffDifferentDepth = 2, coeffDifferentTag = 2, coeffSameTag = 1, coeffStride = 1) {
+            // Score a node based on how much text is directly inside it and its
+            // inline-tag children.
+            function scoreByLength(fnode) {
+                const length = inlineTextLength(fnode.element) * coeffLength;
+                return {
+                    score: length,  // May be scaled someday
+                    note: {inlineLength: length}  // Store expensive inline length for linkDensity().
+                };
+            }
 
-        // This set of rules is the beginning of something that works.
-        // It's modeled after what I do when I try to do this by hand: I look
-        // for balls of black text, and I look for them to be near each other,
-        // generally siblings: a "cluster" of them.
-        const rules = ruleset(
-            // Score on text length -> paragraphish. We start with this
-            // because, no matter the other markup details, the main body text
-            // is definitely going to have a bunch of text.
-            rule(dom('p,div,li,code,blockquote,pre'), props(scoreByLength).type('paragraphish')),
-            // TODO: Maybe include <li>s, blockquotes, and such in here too,
-            // and let the linkDensity and clustering cull out the nav
-            // elements. Or just do a "blur" algorithm within the cluster,
-            // pulling in other elements with decent text density, good CSS
-            // smells, and such. (Interstitials like those probably won't split
-            // clusters if the stride cost is set low enough.) To test, add a
-            // very short paragraph in the midst of the long one, thus testing
-            // our leaning toward contiguousness.
+            // This set of rules is the beginning of something that works.
+            // It's modeled after what I do when I try to do this by hand: I look
+            // for balls of black text, and I look for them to be near each other,
+            // generally siblings: a "cluster" of them.
+            const rules = ruleset(
+                // Score on text length -> paragraphish. We start with this
+                // because, no matter the other markup details, the main body text
+                // is definitely going to have a bunch of text.
+                rule(dom('p,div,li,code,blockquote,pre'), props(scoreByLength).type('paragraphish')),
+                // TODO: Maybe include <li>s, blockquotes, and such in here too,
+                // and let the linkDensity and clustering cull out the nav
+                // elements. Or just do a "blur" algorithm within the cluster,
+                // pulling in other elements with decent text density, good CSS
+                // smells, and such. (Interstitials like those probably won't split
+                // clusters if the stride cost is set low enough.) To test, add a
+                // very short paragraph in the midst of the long one, thus testing
+                // our leaning toward contiguousness.
 
-            // Scale it by inverse of link density:
-            rule(type('paragraphish'), score(fnode => 1 - linkDensity(fnode, fnode.noteFor('paragraphish').inlineLength))),
+                // Scale it by inverse of link density:
+                rule(type('paragraphish'), score(fnode => (1 - linkDensity(fnode, fnode.noteFor('paragraphish').inlineLength)) * coeffLinkDensity)),
 
-            // Give bonuses for being in p tags.
-            rule(dom('p'), score(1.5).type('paragraphish'))
-            // TODO: article tags, etc., too
+                // Give bonuses for being in p tags.
+                rule(dom('p'), score(coeffParagraphTag).type('paragraphish'))
+                // TODO: article tags, etc., too
 
-            // TODO: Ignore invisible nodes so people can't game us with those.
-        );
+                // TODO: Ignore invisible nodes so people can't game us with those.
+            );
 
-        // Return the nodes expressing a document's main textual content.
-        function contentNodes(doc) {
-            const facts = rules.against(doc);
-            const paragraphishes = facts.get(type('paragraphish'));
-            const paragraphishNodes = paragraphishes.map(fnode => fnode.element);
-            const clusts = clusters(
-                paragraphishNodes,
-                3,
-                (a, b) => distance(a, b, {differentDepthCost: 2,
-                                          differentTagCost: 2,
-                                          sameTagCost: 1,
-                                          strideCost: 1
+            // Return the nodes expressing a document's main textual content.
+            function contentNodes(doc) {
+                const facts = rules.against(doc);
+                const paragraphishes = facts.get(type('paragraphish'));
+                const paragraphishNodes = paragraphishes.map(fnode => fnode.element);
+                const clusts = clusters(
+                    paragraphishNodes,
+                    3,
+                    (a, b) => distance(a, b, {differentDepthCost: coeffDifferentDepth,
+                                              differentTagCost: coeffDifferentTag,
+                                              sameTagCost: coeffSameTag,
+                                              strideCost: coeffStride
 
-                                          // This is an addition to the distance
-                                          // function which makes nodes that have
-                                          // outlier lengths further away. It's meant to
-                                          // help filter out interstitials like ads.
-                                          // +1 to make a zero difference in length be 0
-                                          // /10 to bring (only) large differences in length into scale with the above costs
-                                          // additionalCost: (a, b) => Math.log(Math.abs(a.noteFor('paragraphish').inlineLength -
-                                          //                                             b.noteFor('paragraphish').inlineLength) / 10 + 1)
-                                          // TODO: Consider a logistic function instead of log.
-                }));
-            // TODO: Probably promote someting like a "bestCluster()" to an in-
-            // ruleset aggregate function so its output can feed into other
-            // rules. It should take cost coefficients without requiring
-            // distance() itself to be wrapped and passed in.
+                                              // This is an addition to the distance
+                                              // function which makes nodes that have
+                                              // outlier lengths further away. It's meant to
+                                              // help filter out interstitials like ads.
+                                              // +1 to make a zero difference in length be 0
+                                              // /10 to bring (only) large differences in length into scale with the above costs
+                                              // additionalCost: (a, b) => Math.log(Math.abs(a.noteFor('paragraphish').inlineLength -
+                                              //                                             b.noteFor('paragraphish').inlineLength) / 10 + 1)
+                                              // TODO: Consider a logistic function instead of log.
+                    }));
+                // TODO: Probably promote someting like a "bestCluster()" to an in-
+                // ruleset aggregate function so its output can feed into other
+                // rules. It should take cost coefficients without requiring
+                // distance() itself to be wrapped and passed in.
 
-            // Tag each cluster with the total of its paragraphs' scores:
-            const clustsAndSums = clusts.map(clust => [clust,
-                                                       sum(clust.map(para => facts.get(para).scoreFor('paragraphish')))]);
-            // TODO: Make clusters() take fnodes, not nodes, so we can call
-            // scoreFor directly on the fnode above.
-            // TODO: Once that's done, use score as part of the distance metric,
-            // which should tend to push outlier-sized paragraphs out of clusters,
-            // especially if they're separated topographically (like copyright
-            // notices).
-            const bestClust = max(clustsAndSums, clustAndSum => clustAndSum[1])[0];
-            return domSort(bestClust);
-            // Other ideas: We could pick the cluster around the highest-scoring
-            // node (which is more like what Readability does) or the highest-
-            // scoring cluster by some formula (num typed nodes * scores of the
-            // nodes), and contiguous() it so things like ads are excluded but
-            // short paragraphs are included.
+                // Tag each cluster with the total of its paragraphs' scores:
+                const clustsAndSums = clusts.map(clust => [clust,
+                                                           sum(clust.map(para => facts.get(para).scoreFor('paragraphish')))]);
+                // TODO: Make clusters() take fnodes, not nodes, so we can call
+                // scoreFor directly on the fnode above.
+                // TODO: Once that's done, use score as part of the distance metric,
+                // which should tend to push outlier-sized paragraphs out of clusters,
+                // especially if they're separated topographically (like copyright
+                // notices).
+                const bestClust = max(clustsAndSums, clustAndSum => clustAndSum[1])[0];
+                return domSort(bestClust);
+                // Other ideas: We could pick the cluster around the highest-scoring
+                // node (which is more like what Readability does) or the highest-
+                // scoring cluster by some formula (num typed nodes * scores of the
+                // nodes), and contiguous() it so things like ads are excluded but
+                // short paragraphs are included.
+            }
+
+            return contentNodes;
         }
 
         // ---------------------- Test helper routines: -----------------------
@@ -348,7 +353,7 @@ describe('Design-driving demos', function () {
              * This will get continually pickier over time as we run up against
              * the limits of its discriminatory power.
              */
-            function compare(expectedDom, sourceDom) {
+            function compare(expectedDom, sourceDom, contentNodes) {
                 // Currently, this is just a surrounding-whitespace-insensitive
                 // comparison of the text content.
                 const expectedText = trimLines(textContent(expectedDom));
@@ -370,14 +375,29 @@ describe('Design-driving demos', function () {
                         domFromFile('source.html')];
             }
 
-            function readabilityTest(name) {
-                it(name, () => compare(...expectedAndSourceDocs(name)));
+            function readabilityTest(name, compare) {
+                compare(...expectedAndSourceDocs(name));
             }
 
-            readabilityTest('basic-tags-cleaning');
-            readabilityTest('001');
-            readabilityTest('002');  // hellish number of candidate tags. Takes 14s.
-            readabilityTest('daringfireball-1');
+            function fitness(coeffs) {
+                console.log(coeffs);
+                lengthOfExpectedTexts = 0;
+                lengthOfDiffs = 0;
+                const contentNodes = contentNodesWithCoefficients(...coeffs);
+                const cmp = (expectedDom, sourceDom) => compare(expectedDom, sourceDom, contentNodes);
+                readabilityTest('basic-tags-cleaning', cmp);
+                readabilityTest('001', cmp);
+                //readabilityTest('002');  // hellish number of candidate tags. Takes 14s.
+                readabilityTest('daringfireball-1', cmp);
+                console.log(diffScore());
+                return diffScore();
+            }
+
+            it.only('puts the lotion on its skin', function () {
+                console.log('Solution: ');
+                //console.log(fitness([1, 1.5, 1, 2, 2, 1, 1]));
+                console.log(minimize_Powell(fitness, [1,2,3,4,5,6,7]));
+            });
 
             after(function () {
                 const score = diffScore();
