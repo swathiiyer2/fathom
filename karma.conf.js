8095cc3b87ef52bd019a4db1f5f308657d42d655
Swathi Iyer <swathii@stanford.edu>
2017-06-30 13:50:04 -0700

    karma config

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,16 @@
 // Generated on Fri Jun 30 2017 10:39:26 GMT-0700 (PDT)
 module.exports = function(config) {
   config.set({
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['browserify', 'mocha', 'chai'],
     files: ['test/**/*.js'],
+    preprocessors: {
+      'test/**/*.js': [ 'browserify' ],
+    },
+
+    browserify: {
+      debug: true
+    },
+
     reporters: ['progress'],
     port: 9876,  // karma web server port
     colors: true,
